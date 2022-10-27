df8c95a2f2625616a4480f64cc05093f9d5c50b8
Игнат Морозов <gagaved@hotmail.com>
2022-10-27 18:11:17 +1000

    Init

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'app_func/app_in.dart';
+
+//Стандартная точка входа нашего приложения
+//Вызываем функцию запуска
+//Точка входа описана в отдельном файле в app_func.
+
+void main() {
+  //Парсинг JSON
+  runApp(const Application());
+}
